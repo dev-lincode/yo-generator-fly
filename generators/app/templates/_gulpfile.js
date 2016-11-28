8050f18a7ce81526c2ba01650abef8f38f206ce8
Fernando Cezar Chaves <fernandocchaves@gmail.com>
2016-11-28 00:40:52 -0200

    browserSync dependences and configs

--- generators/app/templates/_gulpfile.js
+++ generators/app/templates/_gulpfile.js
@@ -1,37 +1,33 @@
 var gulp = require('gulp'),
-	rename = require("gulp-rename"),
-	uglify = require('gulp-uglify'),
-	concat = require('gulp-concat'),
-	less = require('gulp-less'),
-	minifyCSS = require('gulp-minify-css');
+  rename = require("gulp-rename"),
+  uglify = require('gulp-uglify'),
+  concat = require('gulp-concat'),
+  less = require('gulp-less'),
+  minifyCSS = require('gulp-minify-css'),
+  php = require('gulp-connect-php'),
+  browserSync = require('browser-sync'),
+  sourcemaps = require('gulp-sourcemaps');
 
 var sourcePath = 'assets/src/';
 var destPath = 'assets/';
-var bootstrapPath = sourcePath + 'less/bootstrap/';
 
 gulp.task('css', function(){
-	gulp.src( sourcePath + 'less/css/style.less' )
-	  .pipe( less() )
-	  .pipe(gulp.dest( destPath + 'css') )
-	  .pipe( minifyCSS() )
-	  .pipe(rename( "style.min.css" ))
-	  .pipe(gulp.dest( destPath + 'css' ));
-});
-
-gulp.task('bootstrap', function(){
-	gulp.src( bootstrapPath + 'bootstrap.less' )
-	  .pipe(less())
-	  .pipe(gulp.dest( destPath + 'css' ))
-	  .pipe(minifyCSS())
-	  .pipe(rename( "bootstrap.min.css" ))
-	  .pipe(gulp.dest( destPath + 'css' ));
+  gulp.src( sourcePath + 'less/css/style.less' )
+    .pipe(sourcemaps.init())
+    .pipe( less() )
+    .pipe( minifyCSS() )
+    .pipe(sourcemaps.write())
+    .pipe(rename( "style.min.css" ))
+    .pipe(gulp.dest( destPath + 'css' ))
+    .pipe(browserSync.stream());
 });
 
 gulp.task('js', function(){
-	gulp.src( sourcePath + 'js/scripts.js' )
-		.pipe(uglify())
-		.pipe(rename("scripts.min.js"))
-		.pipe(gulp.dest( destPath + 'js'));
+  gulp.src( sourcePath + 'js/scripts.js' )
+    .pipe(uglify())
+    .pipe(rename("scripts.min.js"))
+    .pipe(gulp.dest( destPath + 'js'))
+    .pipe(browserSync.stream());
 });
 
 gulp.task('watch', function () {
@@ -39,5 +35,25 @@ gulp.task('watch', function () {
     gulp.watch([ sourcePath + 'js/**/*.js'], ['js']);
 });
 
-gulp.task('build', ['css', 'bootstrap', 'js']);
+gulp.task('php', function() {
+  php.server({ base: 'project-name', port: 80, keepalive: true});
+});
+
+gulp.task('browser-sync',['php'], function() {
+  browserSync({
+    proxy: '127.0.0.1/project-name',
+    port: 8080,
+    open: true,
+    notify: false
+  });
+});
+
+gulp.task('watch-dev', ['browser-sync'], function () {
+  gulp.watch(destPath + 'css/style.min.css').on('change', function () {browserSync.reload();});
+  gulp.watch(destPath + 'js/scripts.min.js').on('change', function () {browserSync.reload();});
+  gulp.watch('**/*.php').on('change', function () {browserSync.reload();});
+});
+
+gulp.task('build', ['css', 'js']);
 gulp.task('default', ['build', 'watch']);
+gulp.task('dev', ['build', 'watch', 'watch-dev']);
